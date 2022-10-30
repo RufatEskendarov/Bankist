91993146ec43a5be7a02f2cbb68161e98cea564d
Rufat Eskendarov <rufat.eskendarov@gmail.com>
2022-10-30 20:55:44 -0300

    Updated full pack

---
+++ jstraning.js
@@ -0,0 +1,24 @@
+'use strict';
+// const num = 388894526.23;
+// const options = {
+//   style: 'currency',
+//   unit: 'celsius',
+//   currency: 'EUR',
+//   //   useGrouping: false,
+// };
+// console.log('AZE:   ', new Intl.NumberFormat('az-AZ', options).format(num));
+// console.log('GER:   ', new Intl.NumberFormat('de-DE', options).format(num));
+// console.log(
+//   `${navigator.language}:`,
+//   new Intl.NumberFormat(navigator.language, options).format(num)
+// );
+// const ingrid = ['balls', 'cucumer'];
+// const ass = setTimeout(
+//   (ing1, ing2) => console.log(`Here fuck with ${ing1} and ${ing2}`),
+//   3000,
+//   ...ingrid
+// );
+// console.log('Waiting...');
+// if (ingrid.includes('balls')) clearTimeout(ass);
+
+// setInterval(function () {});
